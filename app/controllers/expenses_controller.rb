2b97c33ca76364828f1a18591487a6f2a82f483d
sebasdeldi <sebasdeldi123@gmail.com>
2016-03-09 23:48:19 -0500

    Jj

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,18 +1,7 @@
 class ExpensesController < ApplicationController
-  attr_accessor :input_cate
 
   def index
-     # @expenses = Expense.order("date DESC")
-     input_cate = params[:category_id]
-     input_conc = params[:concept]
 
- 	
-
-	exp1= Expense.where("concept like ?", "%#{input_conc}%")
-	exp2= Expense.where(category_id: params[:category_id])
-
-	@expenses = (exp1 << exp2).flatten!.uniq
+	@expenses= Expense.where("concept like ?", "%#{params[:concept]}%" ).where(category_id: params[:category_id] )
   end
-
 end
-
